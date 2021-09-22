60dd5cd8be73b99164e627570b75bd2a246ebd8e
Milad Nekofar <milad@nekofar.com>
2021-09-22 08:11:59 +0330

    test: add a test to check it can send multiple requests

---
+++ tests/MockClient.php
@@ -0,0 +1,13 @@
+<?php
+
+use GuzzleHttp\Psr7\Request;
+use function Nekofar\Pest\MockClient\mockClient;
+
+it('can send multiple requests', function ():void {
+    $request = new Request('GET', '/');
+
+    mockClient()->sendRequest($request);
+    mockClient()->sendRequest($request);
+
+    expect(mockClient()->getRequests())->toHaveCount(2);
+});
